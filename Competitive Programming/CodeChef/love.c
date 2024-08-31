4c10a92d35f23271ebcf0d1626476b7d7e67a7de
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-08-31 20:05:18 +0530

    few changes

--- Competitive Programming/CodeChef/love.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("I e you Ankit");
-    return 0;
-}
--- Competitive Programming/CodeForces/softDrink.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n, k, l, c, d, p, nl, np;
-    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
-    int res = min((k*l)/nl, min(d, p/np));
-    cout << res/n << endl;
-}
